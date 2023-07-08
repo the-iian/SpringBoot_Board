79293550da926eaee41cd6762d04e5b0341b18b1
iian <dheyw@naver.com>
2023-07-08 16:38:21 +0900

    마크다운 컴포넌트 작성

---
+++ src/main/java/com/mysite/sbb/CommonUtil.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb;
+
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+
+@Component
+public class CommonUtil {
+    public String markdown(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+}
